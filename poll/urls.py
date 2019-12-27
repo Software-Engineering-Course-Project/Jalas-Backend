a17b52a0f5c9e3b33ff7a63a60697e0f8bfb4fca
Mohammadhadi Omidi <mohammadhadi.omidi95@gmail.com>
2019-12-27 13:32:12 +0330

    remove slash form urls of add comment

--- poll/urls.py
+++ poll/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('vote/<int:poll_id>', views.VotingView.as_view()),
     path('get_voter/<int:poll_id>', views.GetVoterName.as_view()),
     path('get_last_poll/', views.GetLastPoll.as_view()),
-    path('add_comment/<int:poll_id>/', views.AddCommentView.as_view()),
+    path('add_comment/<int:poll_id>', views.AddCommentView.as_view()),
     path('modify/<int:poll_id>', views.ModifiedPollView.as_view()),
     path('get_comment/<int:poll_id>', views.GetCommentView.as_view()),
 ]
