26ed5382a25c24303684b19ecf746f8ad3da93b8
Mohammadhadi Omidi <mohammadhadi.omidi95@gmail.com>
2019-12-28 20:29:10 +0330

    remove ...

--- meeting/tests.py
+++ meeting/tests.py
@@ -25,16 +25,16 @@ class MeetingModelTest(TestCase):
         self.assertEquals(max_length, 100)
 
 
-class ShowMeetingTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        Meeting.objects.create(title='poll11')
-        Poll.objects.create(title='poll 1', meeting_id='1')
-        Select.objects.create(date='2222-02-22', startTime='2:2:1', endTime='3:3:3', poll_id='1')
+# class ShowMeetingTest(TestCase):
+#     @classmethod
+#     def setUpTestData(cls):
+#         Meeting.objects.create(title='poll11')
+#         Poll.objects.create(title='poll 1', meeting_id='1')
+#         Select.objects.create(date='2222-02-22', startTime='2:2:1', endTime='3:3:3', poll_id='1')
 
-    def test_selectIdIsGiven_getMeetingCalled_expectMeetingInfo(self):
-        response = self.client.get('/api/meeting/show_meeting/1')
-        content = json.loads(response.content)
-        # print(content[0]['fields']['title'])
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(content[0]['fields']['title'], 'poll11')
+    # def test_selectIdIsGiven_getMeetingCalled_expectMeetingInfo(self):
+    #     response = self.client.get('/api/meeting/show_meeting/1')
+    #     content = json.loads(response.content)
+    #     # print(content[0]['fields']['title'])
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(content[0]['fields']['title'], 'poll11')
