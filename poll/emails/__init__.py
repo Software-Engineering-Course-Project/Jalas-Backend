bb26e5c582165fe72e82ef9164b3c1b72fb38df7
Mohammadhadi Omidi <mohammadhadi.omidi95@gmail.com>
2020-01-08 19:46:39 +0330

    remove config from emails.

--- poll/emails/__init__.py
+++ poll/emails/__init__.py
@@ -4,7 +4,7 @@ from Jalas import settings
 
 
 def send_email_arrange_meeting(user, title, link, participants):
-    if user.configuration.arrange_meeting and participants:
+    # if user.configuration.arrange_meeting and participants:
         try:
             send_mail(
                 subject=title,
@@ -17,7 +17,7 @@ def send_email_arrange_meeting(user, title, link, participants):
 
 
 def send_email_add_option(user, title, link, participants):
-    if user.configuration.add_option and participants:
+    # if user.configuration.add_option and participants:
         try:
             send_mail(
                 subject=title,
@@ -30,7 +30,7 @@ def send_email_add_option(user, title, link, participants):
 
 
 def send_email_add_participant(user, title, link, participants):
-    if user.configuration.add_new_participant and participants:
+    # if user.configuration.add_new_participant and participants:
         try:
             send_mail(
                 subject=title,
@@ -43,7 +43,7 @@ def send_email_add_participant(user, title, link, participants):
 
 
 def send_email_remove_option(user, title, link, participants):
-    if user.configuration.remove_option and participants:
+    # if user.configuration.remove_option and participants:
         try:
             send_mail(
                 subject=title,
@@ -56,7 +56,7 @@ def send_email_remove_option(user, title, link, participants):
 
 
 def send_email_new_vote(user, poll, to):
-    if user.configuration.remove_option and to:
+    # if user.configuration.remove_option and to:
         try:
             send_mail(
                 subject=poll.title,
@@ -69,7 +69,7 @@ def send_email_new_vote(user, poll, to):
 
 
 def send_email_close_poll(user, title, link, participants):
-    if user.configuration.remove_option and participants:
+    # if user.configuration.remove_option and participants:
         try:
             send_mail(
                 subject=title,
