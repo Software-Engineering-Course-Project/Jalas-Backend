d36e77319ac7b0f90f310d8ffde7f075e96c9238
Mohammadhadi Omidi <mohammadhadi.omidi95@gmail.com>
2020-01-10 17:45:30 +0330

    change <br> to &&

--- logger/views.py
+++ logger/views.py
@@ -33,11 +33,11 @@ class ShowLogs(APIView):
             avg_res_time = self.getAverageResponseTime()
             throughput = self.get_throughput()
 
-            res = "Till now: <br> Average time duration for each reservation is " + str(avg) + ' sec '+\
-                "<br> " + 'Canceled meeting number is ' + str(canceledNumber) +\
-                "<br>" + 'Reserved Room number is ' + str(reservedRoomNum) +\
-                "<br>" + ('Average response time is %2.4f' % avg_res_time) +\
-                '<br>' + 'Throughput is ' + str(throughput)
+            res = "Till now: && Average time duration for each reservation is " + str(avg) + ' sec '+\
+                "&&" + 'Canceled meeting number is ' + str(canceledNumber) +\
+                "&&" + 'Reserved Room number is ' + str(reservedRoomNum) +\
+                "&&" + ('Average response time is %2.4f' % avg_res_time) +\
+                '&&' + 'Throughput is ' + str(throughput)
             return HttpResponse(res)
         return HttpResponse('You can\'t see logs.')
 
@@ -50,6 +50,6 @@ class ShowLogs(APIView):
 
     def get_throughput(self):
         this_time = time.time()
-        throughput_time = 1.0 * 60 * 5
+        throughput_time = 1.0 * 60
         tp = Throughput.objects.filter(create_date__gte=(this_time - throughput_time))
         return len(tp)
