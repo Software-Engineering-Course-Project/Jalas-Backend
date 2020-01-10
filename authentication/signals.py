e0b8494610527c2952578d6a40ae5860ea32188d
Mohammadhadi Omidi <mohammadhadi.omidi95@gmail.com>
2020-01-10 16:29:12 +0330

    rmeove signal

--- authentication/signals.py
+++ authentication/signals.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db.models.signals import post_init
 from django.dispatch import receiver
 
 from authentication import models
 from configurations.models import Configuration
 
 
-@receiver(post_save, sender=models.User)
+@receiver(post_init, sender=models.User)
 def save_user_profile(sender, instance, **kwargs):
-    conf = Configuration(user=instance)
-    conf.save()
-    instance.configuration = conf
+    pass
+    # conf = Configuration(user=instance)
+    # conf.save()
+    # instance.configuration = conf
